d0dc4f3c46f86ad7c75882595481d36dd10e99b0
finn-pal <173330723+finn-pal@users.noreply.github.com>
2024-07-08 21:37:50 +1000

    setting up venv and external import of fire functions such as gizmo

---
+++ gizmo_test.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.path.insert(0, "/Users/z5114326/Documents/ExternalCode")
+
+import gizmo_analysis as gizmo  # type: ignore
+import matplotlib.pyplot as plt
+import numpy as np
+import utilities as ut  # type: ignore
