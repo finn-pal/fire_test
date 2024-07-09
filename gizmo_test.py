97b36189e051f798566e316f176dce8d6b3ab2de
finn-pal <173330723+finn-pal@users.noreply.github.com>
2024-07-10 08:59:01 +1000

    Correcting upload file

--- gizmo_test.py
+++ gizmo_test.py
@@ -1,7 +1,7 @@
 # import sys
 # sys.path.insert(0, "/Users/z5114326/Documents/ExternalCode")
 
-import gizmo_analysis as gizmo  # type: ignore
+import gizmo_analysis as gizmo
 import matplotlib.pyplot as plt
 import numpy as np
-import utilities as ut  # type: ignore
+import utilities as ut
